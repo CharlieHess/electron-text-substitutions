9043c91d8740f5b898cb87c95a4a20b405981c72
Charlie Hess <charlie@slack-corp.com>
2016-06-06 12:10:50 -0700

    Test Harder & lint.

--- test/index.js
+++ test/index.js
@@ -2,8 +2,26 @@ import assert from 'assert';
 import {getSubstitutionRegex} from '../src/text-substitution';
 
 describe('the Text Substitution regex', () => {
-  it('should not match a word without boundaries', () => {
-    let re = getSubstitutionRegex('banana');
-    assert(!'banana'.match(re));
+  it('should only match a word with boundaries', () => {
+    let regex = getSubstitutionRegex('banana');
+    let shouldMatch = [
+      'banana ',
+      'banana-',
+      'banana.',
+      '-banana ',
+      'banana\n',
+      'banana\r.',
+      '\nbanana\t'
+    ];
+
+    let shouldNotMatch = [
+      'banana',
+      ' banana',
+      'bananabanana',
+      'helpimtrappedina-bananafactory '
+    ];
+
+    for (let candidate of shouldMatch) assert(candidate.match(regex));
+    for (let candidate of shouldNotMatch) assert(!candidate.match(regex));
   });
 });
