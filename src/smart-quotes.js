89662507ecd7fd4a2af6f9f344326c7253c5b54c
Charlie Hess <charlie@slack-corp.com>
2016-06-14 22:20:11 -0700

    Try to more closely mimic the NSTextView behavior.

--- src/smart-quotes.js
+++ src/smart-quotes.js
@@ -1,16 +1,14 @@
 /**
- * replaceQuotes - description
+ * Used on input to progressively replace straight quotes with curly quotes.
  *
- * @param  {type} input description
- * @return {type}       description
+ * @param  {String} input The input string
+ * @return {String}       The output string
  */
 export function replaceQuotes(input) {
   return input
-    .replace(/(\W|^)"(\S)/g, '$1\u201c$2')                                       // beginning "
-    .replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2')          // ending "
-    .replace(/([^0-9])"/g,'$1\u201d')                                            // remaining " at end of word
-    .replace(/(\W|^)'(\S)/g, '$1\u2018$2')                                       // beginning '
-    .replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                  // conjunction's possession
-    .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3')                 // ending '
-    .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3'); // abbrev. years like '93
+    .replace(/(\S)"([\S\s])/, '$1”$2')   // closing doubles
+    .replace(/"([\S\s])/, '“$1')         // opening doubles
+    .replace(/(\W|^)'([\w\s])/, '$1‘$2') // opening singles
+    .replace(/([\S\s])'(\s)/, '$1’$2')   // closing singles
+    .replace(/(\w)'(\w+\s)/, '$1’$2');   // contractions
 }
--- test/smart-quotes.js
+++ test/smart-quotes.js
@@ -3,17 +3,27 @@ import {replaceQuotes} from '../src/smart-quotes';
 
 describe('the replaceQuotes method', () => {
   it('should replace straight quotes with curly quotes', () => {
-
     let testCases = [
       { input: `\"`, output: `\"` },
-      { input: `\"\"`, output: `“”` },
+      { input: `\"\"`, output: `“\"` },
+      { input: `“\"\"`, output: `“”\"` },
+      { input: `“”\" `, output: `“”” ` },
+      { input: `“ \" `, output: `“ “ ` },
+      { input: `a\"b`, output: `a”b` },
+      { input: `\"a\" `, output: `“a” ` },
+      { input: `\n\"\n`, output: `\n“\n` },
+
       { input: `\'`, output: `\'` },
-      { input: `\'\'`, output: `‘’` },
-      { input: `\'some text\'`, output: `‘some text’` },
-      { input: `\"more text\"`, output: `“more text”` },
-      { input: `\"\'inner quote\'`, output: `“‘inner quote’` },
-      { input: `\"\'what\'s that,\' she said\"`, output: `“‘what’s that,’ she said”` },
-      { input: `Hot Since \'86`, output: `Hot Since ’86` }
+      { input: `\'\'`, output: `\'\'` },
+      { input: `\'a`, output: `‘a` },
+      { input: `\'a\' `, output: `‘a’ ` },
+      { input: `can\'t`, output: `can\'t` },
+      { input: `can\'t `, output: `can’t ` },
+
+      { input: `\"\'who?`, output: `“‘who?` },
+      { input: `\"O\'Doyle rules!\" `, output: `“O’Doyle rules!” ` },
+      { input: `\"\'what\'s that,\' she said\" `, output: `“‘what’s that,’ she said” ` },
+      { input: `end of one\" \"start of another`, output: `end of one” “start of another` }
     ];
 
     for (let {input, output} of testCases) {
