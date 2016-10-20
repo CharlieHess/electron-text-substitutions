adb4270c97c567680f3c6d241e000c8fd73c2123
Charlie Hess <charlie@slack-corp.com>
2016-10-20 14:01:37 -0700

    Use the public SerialSubscription instead of our own.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/debounceTime';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
-import {SerialSubscription} from './serial-subscription';
+import SerialSubscription from 'rxjs-serial-subscription';
 import {getSubstitutionRegExp, getSmartQuotesRegExp, getSmartDashesRegExp,
   scrubInputString, formatReplacement, regExpReplacer, regExpReviver} from './regular-expressions';
 import {isUndoRedoEvent, isBackspaceEvent} from './keyboard-utils';
